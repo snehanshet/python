9f7d7b363d7b36787ac09f46ead984edd22c87eb
Sneha N Shet <snehanshet1999@gmail.com>
2020-07-20 17:33:08 +0530

    Add files via upload

---
+++ forth.py
@@ -0,0 +1,15 @@
+while True:
+    line=input('> ')
+    if line[0]=='#':
+        continue
+    if line=='done':
+        break
+    print(line)
+print('Done!')
+#second code
+while True:
+    line=input('>')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
