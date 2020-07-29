9624109000b46d7c92e0d564d5da170bf39f89d2
Sneha N Shet <snehanshet1999@gmail.com>
2020-07-29 17:33:28 +0530

    Update urllib.py

--- urllib.py
+++ urllib.py
@@ -1,3 +1,4 @@
+#since HTTP is so common ,we have a library that does all the socket work for us and makesweb pages look like a file
 import urllib.request , urllib.parse , urllib.error
 fhand=urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
